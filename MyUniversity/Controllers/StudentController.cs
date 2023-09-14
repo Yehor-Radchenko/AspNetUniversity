88664aafdf41f3ac015b0fef72375b15090b53a8
Yehor-Radchenko <115692958+Yehor-Radchenko@users.noreply.github.com>
2023-09-14 11:50:27 +0300

    fixed Update exception working with fk-containing entities (groups and students)

--- MyUniversity/Controllers/StudentController.cs
+++ MyUniversity/Controllers/StudentController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyUniversity.Data;
+using MyUniversity.Models;
 
 namespace MyUniversity.Controllers
 {
@@ -11,6 +13,68 @@ namespace MyUniversity.Controllers
         public StudentController(MyUniversityDbContext context)
         {
             _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Students
+            .Include(s => s.Group)
+            .ToList());
+        }
+
+        public async Task<IActionResult> Create(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id is not null)
+            {
+                Student student = new Student { Id = id.Value };
+                _context.Entry(student).State = EntityState.Deleted;
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+
+                Student? student = await _context.Students.FirstOrDefaultAsync(p => p.Id == id);
+
+                if (student is not null)
+                {
+                    var allGroups = _context.Groups.ToList();
+
+                    var groupSelectList = allGroups.Select(group => new SelectListItem
+                    {
+                        Text = group.Name,
+                        Value = group.Id.ToString(),
+                        Selected = group.Id == student.Group.Id
+                    }).ToList();
+
+                    ViewBag.AllGroups = groupSelectList;
+
+                    return View(student);
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student student)
+        {
+            _context.Students.Update(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
